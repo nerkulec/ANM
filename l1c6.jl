e2fbeb80b1246dd037f7ffc410f803718afe5852
nerkulec <nerkulec@github.com>
2019-10-10 12:05:09 +0200

    x is Float64

--- l1c6.jl
+++ l1c6.jl
@@ -1,14 +1,13 @@
 function float_from_bitstring(bitstr)
-    one_indexing_hurts_my_brain = 1
-    sign = 1-2*parse(Int64, bitstr[0+one_indexing_hurts_my_brain])
+    sign = 1-2*parse(Int64, bitstr[0+1])
     c = Int64(0)
     for i in 1:11
-        c += parse(Int64, bitstr[i+one_indexing_hurts_my_brain])*2^(11-i)
+        c += parse(Int64, bitstr[i+1])*2^(11-i)
     end
     c -= 1023
     m = Float64(1.0)
     for i in 12:63
-        m += parse(Int64, bitstr[i+one_indexing_hurts_my_brain])*Float64(2.0)^(-i+11)
+        m += parse(Int64, bitstr[i+1])*Float64(2.0)^(-i+11)
     end
     return sign*m*2^c
 end
--- l1c7.jl
+++ l1c7.jl
@@ -2,7 +2,7 @@ function f(x)
     return x*(Float64(1.0)/x)
 end
 
-for x in LinRange(1,2,10000)
-    print("f(",x,")=")
+for x in LinRange(1,2,101)
+    print("f(",Float64(x),")=")
     print(f(x),'\n')
 end
